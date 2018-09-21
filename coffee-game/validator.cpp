2ef438700d90ddca9f2dd177312be4aa22b71e49
eggeek <eggeek.me@gmail.com>
2018-09-21 15:14:53 +1000

    add `coffee-game`

---
+++ coffee-game/validator.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+int n;
+
+int main(int argc, char* argv[]) {
+    registerValidation(argc, argv);
+    n = inf.readInt(1, 3000, "n");
+    inf.readEoln();
+    inf.readEof();
+}
