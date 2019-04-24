b277bea3066202212074733557ad7ef62f798419
eggeek <eggeek.me@gmail.com>
2019-04-24 17:26:17 +1000

    add `validator.cpp,checker.cpp,gen.cpp` in template

---
+++ resources/validator.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include "testlib.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    registerValidation(argc, argv);
+    int n = inf.readInt(2, 100, "n");
+    inf.readSpace();
+    inf.readInt(2, min(n, 26), "k");
+    inf.readEoln();
+    inf.readEof();
+}
