6cf08a79c8f4898af5b491734f03545295d38080
eggeek <eggeek.me@gmail.com>
2019-08-20 10:40:29 +1000

    rewrite statement `buildingdays`

--- buildingdays(overtune)/eggeek_main.cpp
+++ buildingdays(overtune)/eggeek_main.cpp
@@ -13,7 +13,7 @@ void draw(int x1, int y1, int x2, int y2) {
 bool isTraversable(int x, int y) {
   if (x < 0 || x >= w) return false;
   if (y < 0 || y >= h) return false;
-  if (g[y][x] == '#') return false;
+  if (g[y][x] == '=') return false;
   return true;
 }
 
@@ -45,17 +45,25 @@ bool isX(int x, int y) {
   else return false;
 }
 
+void dfs(int x, int y) {
+  if (0 <= x && x < w && 0 <= y && y < h && g[y][x] == '=') {
+    g[y][x] = '#';
+    dfs(x, y-1); dfs(x, y+1);
+    dfs(x-1, y); dfs(x+1, y);
+  }
+}
+
 int main() {
   cin >> w >> h >> n;
   memset(g, 0, sizeof(g));
 
   for (int i=0; i<h; i++)
-    for (int j=0; j<w; j++) g[i][j] = '#';
+    for (int j=0; j<w; j++) g[i][j] = '=';
 
   for (int i=0; i<n; i++) {
     int x1, y1, x2, y2;
     cin >> x1 >> y1 >> x2 >> y2;
-    draw(min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2));
+    draw(x1, y1, x2, y2);
   }
 
   for (int i=0; i<w; i++)
@@ -68,5 +76,15 @@ int main() {
     for (int j=0; j<h; j++) if (g[j][i] == '.') {
       if (isX(i, j)) g[j][i] = '+';
     }
+
+  for (int i=0; i<h; i++) {
+    if (g[i][0] == '=')   dfs(0, i);
+    if (g[i][w-1] == '=') dfs(w-1, i);
+  }
+  for (int i=0; i<w; i++) {
+    if (g[0][i] == '=')   dfs(i, 0);
+    if (g[h-1][i] == '=') dfs(i, h-1);
+  }
+
   for (int i=0; i<h; i++) printf("%s\n", g[i]);
 }
